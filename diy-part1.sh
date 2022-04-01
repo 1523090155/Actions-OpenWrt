0213853ee5aa92b468f08fc7df0cfdeaf9c9695d
1523090155 <59162532+1523090155@users.noreply.github.com>
2022-04-01 09:14:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,7 +3,7 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-# Lisence: MIT
+# Lisence: MIT 
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
